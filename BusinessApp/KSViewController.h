f903010d4e418e447a1beebeec1d3b31d5736bb9
crazyer02 <crazyer02@gmail.com>
2014-10-21 23:55:32 +0800

    加了登录按钮，访问WCF还没有完成

--- BusinessApp/KSViewController.h
+++ BusinessApp/KSViewController.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "ASIHttpHeaders.h"
+
 
 @interface KSViewController : UIViewController
 
--- BusinessApp/KSViewController.m
+++ BusinessApp/KSViewController.m
@@ -19,6 +19,28 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)doLoginBtn:(UIButton *)sender {
+    NSURL *url = [NSURL URLWithString: @ "http://192.268.0.11:9422/Service1.svc/LogInf/111/111"];
+    ASIHTTPRequest *request = [ASIHTTPRequest requestWithURL:url];
+    [request setRequestMethod:@"POST"];
+    
+    [request startSynchronous];
+    
+    NSError *error = [request error];
+    
+    if (!error) {
+        NSString *response = [request responseString];
+        
+        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"测试"
+                                                            message:response
+                                                           delegate:nil
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+        [alertView show];
+        [alertView release];
+        
+    }
+}
 
 - (void)didReceiveMemoryWarning
 {
